057347bfd706855c1faaa7420653bb245412434f
fajar17ibrahim <fajar17ibrahim@gmail.com>
2020-04-25 23:12:38 +0700

    update icon laucher and splash screen

---
+++ app/src/main/java/com/autoresto/ui/splash/Splash.java
@@ -0,0 +1,45 @@
+package com.autoresto.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.autoresto.MainActivity;
+import com.autoresto.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ImageView imageView = (ImageView) findViewById(R.id.logo);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.translation);
+        imageView.startAnimation(animation);
+        final Intent intent = new Intent(this, MainActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
